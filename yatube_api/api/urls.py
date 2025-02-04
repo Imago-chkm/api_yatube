3bf582e2d7849d6bec86cd3773b109256ec9b230
Geychenko Mikhail <maikl-g95@yandex.ru>
2025-02-04 19:16:01 +0300

    заработали пути в приложении api

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,13 +1,38 @@
-from django.urls import include, path
 from rest_framework import routers
+from django.urls import path, re_path
+from rest_framework.authtoken import views
 
 from api.views import CommentViewSet, GroupViewSet, PostViewSet
 
 app_name = 'api'
 
+comment_list = CommentViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+comment_detail = CommentViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
 router = routers.SimpleRouter()
-router.register(r'comments', CommentViewSet)
 router.register(r'group', GroupViewSet)
 router.register(r'posts', PostViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    re_path(
+        r'^posts/(?P<post_id>\d+)/comments/$',
+        comment_list,
+        name='comment-list'
+    ),
+    re_path(
+        r'^posts/(?P<post_id>\d+)/comments/(?P<comment_id>\d+)/$',
+        comment_detail,
+        name='comment-detail'
+    ),
+    path('api-token-auth/', views.obtain_auth_token),
+]
+urlpatterns += router.urls
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -6,13 +6,34 @@ from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        """Фильтрация комментариев по post_id."""
+
+        post_id = self.kwargs.get('post_id')
+        if post_id:
+            return Comment.objects.filter(post_id=post_id)
+        return Comment.objects.all()
+
+    def perform_update(self, serializer):
+        """Проверка прав на изменение контента."""
+
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+    def perform_update(self, serializer):
+        """Проверка прав на изменение контента."""
+
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -20,6 +41,8 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
 
     def perform_update(self, serializer):
+        """Проверка прав на изменение контента."""
+
         if serializer.instance.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
         super(PostViewSet, self).perform_update(serializer)
